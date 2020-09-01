82f1db68a7b42c56092e8a75d93b304d51bd53a8
Leandro Galo <leandro-galo@hotmail.com>
2020-09-01 14:22:38 -0600

    This is my Script for Final Assignament

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,6 @@
 ### Preparing Data
+library(dplyr)
+
 
 filename <- "FinalTestData.zip"
 
@@ -15,12 +17,19 @@ if (!file.exists("UCI HAR Dataset")) {
 
 
 features <- read.table("UCI HAR Dataset/features.txt", col.names = c("n","functions"))
+
 activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
+
 subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
+
 x_test <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = features$functions)
+
 y_test <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "code")
+
 subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
+
 x_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
+
 y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
 
 
